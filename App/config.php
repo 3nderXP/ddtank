1dfe8d69c34013e9dfbf4a440e08380fef6fb754
3nderXP <adrian08cavalcante09santos@gmail.com>
2025-02-27 06:36:55 -0300

    Fixing project files data

--- App/config.php
+++ App/config.php
@@ -4,7 +4,7 @@ session_start();
 
 define("SITE_PROTOCOL", isset($_SERVER["HTTPS"]) ? "https://" : "http://");
 define("SITE_DOMAIN", $_SERVER["SERVER_NAME"]);
-define("SITE_ROOT", "/ddtank_paraguai");
+define("SITE_ROOT", "/ddtank");
 
 define("URL_BASE", SITE_PROTOCOL.SITE_DOMAIN.SITE_ROOT);
 
@@ -12,3 +12,6 @@ define("SITE_NAME", "DDTank Paraguai");
 define("SITE_LANG", "pt-BR");
 
 define("VERSION", "0.3.0");
+
+ini_set("display_errors", 1);
+ini_set("error_reporting", E_ALL);
